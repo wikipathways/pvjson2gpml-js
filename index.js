a7eec4f5a551cb124f623eada4ebd68af09f300e
Anders Riutta <git@andersriutta.com>
2014-10-01 17:54:10 -0700

    First commit. Initial proof of concept works.

---
+++ index.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+  , pvjson = require('./WP1.json')
+  , pd = require('pretty-data').pd
+  //, Ractive = require('ractive');
+  // using a modified version of ractive that has the rdf namespace added
+  , Ractive = require('./ractive.js')
+  ;
+
+var ractive = new Ractive({
+  template: fs.readFileSync('./gpml-2013a-template.xml', {encoding: 'utf8'})
+  , data: { pvjson: pvjson }
+});
+
+var gpml = ractive.toHTML();
+gpml = pd.xml(gpml);
+var lineDelimitedGpml = gpml.split('\n');
+gpml = lineDelimitedGpml.slice(1, lineDelimitedGpml.length - 1).join('\n');
+console.log(gpml);
